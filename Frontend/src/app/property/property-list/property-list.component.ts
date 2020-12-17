65e1b76642b672f556ac8949214444944dc14c48
Mahmoud <mahmoud.barakat.dh@gmail.com>
2020-12-17 14:31:32 +0100

    55

---
+++ Frontend/src/app/property/property-list/property-list.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HousingService } from 'src/app/services/housing.service';
+
+
+
+
+@Component({
+  selector: 'app-property-list',
+  templateUrl: './property-list.component.html',
+  styleUrls: ['./property-list.component.css']
+})
+export class PropertyListComponent implements OnInit {
+  properties: Array<any> = [];
+
+  constructor(private housingService: HousingService) { }
+
+  ngOnInit(): void {
+   this.housingService.getAllProperties().subscribe(
+     data=>{
+       this.properties=data;
+       console.log(data);
+      },error => {
+        console.log('httperror');
+        console.log(error);
+      }
+
+
+   );
+  }
+
+}
